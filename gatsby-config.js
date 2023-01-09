6babda6722978f43d7e09d0627b5b2f5f596d79e
OndrejHj04 <ondra.hajku@seznam.cz>
2023-01-09 21:15:50 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,7 @@
  */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-  ],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
