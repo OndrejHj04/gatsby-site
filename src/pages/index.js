c45a0a1cc8de1f7856a67f8379c47b8408269d78
OndrejHj04 <ondra.hajku@seznam.cz>
2023-01-10 21:44:30 +0100

    display data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import * as React from "react";
-import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is Gatsby</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+
     </Layout>
   );
 };
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <Seo title="Home Page" />;
 
 // Step 3: Export your component
 export default IndexPage;
